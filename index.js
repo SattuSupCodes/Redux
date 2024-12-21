87947473f09f5ff1fe3a82e43cf875c4d08d2bea
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-12-21 19:48:27 +0530

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log('from index.js')
+const BUY_CAKE = 'BU_CAKE'
+// {
+//     type: BUY_CAKE
+//     info: 'First redux action'
+// }
+
+function buyCake(){
+    return{
+        type: BUY_CAKE,
+        info: 'First redux action'
+    } 
+}
